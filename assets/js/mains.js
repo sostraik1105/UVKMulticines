c6ab9fd5d0ce0a93a5c03ed16e98d5a06f2df5ca
sostraik1105 <focusdani1105@gmail.com>
2022-03-01 12:47:22 -0500

    f :c

--- assets/js/mains.js
+++ assets/js/mains.js
@@ -1,1 +1,20 @@
 import datos from "../../data.json" assert {type: "json"};
+
+const cards = ()=>{
+    const container = document.getElementById('pelis');
+    const cardPelis = document.getElementById('cardPelis').content;
+    const fragment = document.createDocumentFragment();
+
+    datos.forEach(el=>{
+        cardPelis.querySelector("img").setAttribute("src", el.img);
+        cardPelis.querySelector("h3").textContent = el.title;
+        cardPelis.querySelector("p").textContent = `${el.genero}, ${el.duracion}, ${el.rangoEdad}`;
+
+        let repeat = document.importNode(cardPelis, true)
+        fragment.appendChild(repeat)
+    })
+
+    container.appendChild(fragment)
+}
+
+document.addEventListener("DOMContentLoaded", cards )
